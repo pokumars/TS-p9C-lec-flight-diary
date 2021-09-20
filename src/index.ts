130fb2e48c6df6e7d431fed6baea24ad7ca79247
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-09-20 05:54:22 +0300

    create services, routes directories

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
+import diaryRouter from './routers/diaries';
 
 const app = express();
 app.use(express.json());
 
-
+app.use('/api/diaries', diaryRouter);
 
 app.get('/ping', (_req, res) => {
   console.log('someone Pinged here');
 
-  res.send('pong');
+  res.send('flight diary backend pongs back');
 });
 
 const PORT = 3000;
